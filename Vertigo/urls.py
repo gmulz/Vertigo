5d83e400a02764524b12ec7d275778c468b2eb26
Michael Luo <mrl233@cornell.edu>
2015-01-17 16:23:01 -0500

    Trying different settings

--- Vertigo/urls.py
+++ Vertigo/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'Vertigo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', static.Vertigo.static.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
